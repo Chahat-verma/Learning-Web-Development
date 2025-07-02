c01e7c6271ee9de0826050cdc40f709745703255
Chahat verma <chahatverma360@gmail.com>
2025-07-02 18:59:20 +0530

    Day-57 of learning

---
+++ video 57/index.js
@@ -0,0 +1,44 @@
+console.log("Hello this is tutuorial on loops in js");
+/*
+loops in js are of 5 type
+for loop  - simple loop
+for in loop  - used for objects to print key or value
+foreach loop  - we study it later
+for of loop  - used as iterator in arrays
+while loop  - simple loop does not need to initialize directly check the condition
+do-while loop  - same as while but it print the value first then check the condition not gernally used
+*/
+
+// let a = 2;
+// for (let i = 0; i < 1000; i++) {
+//     console.log(i % a);
+// }
+
+// let as = {
+//   Name: "Raju",
+//   Role: "Devloper",
+//   Salary: "90 lakhs",
+// };
+// for (const key in as) {
+// if (Object.prototype.hasOwnProperty.call(object, key)) {
+//     const element = as[key];
+//     console.log(key, element);
+// }
+
+// for (const c of "Chahat verma"){
+//     console.log(c);
+// }
+
+// let i = 2;
+// while (i < 1000) {
+//     console.log(i);
+//     i++;
+// }
+
+
+let i = 25;
+do{
+    console.log(i);
+    i++;
+}
+while (i < 10);
