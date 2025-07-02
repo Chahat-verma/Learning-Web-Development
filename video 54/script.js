b5b3ba93de71ea6e018d251c0d5cf61aa0aa3902
Chahat verma <chahatverma360@gmail.com>
2025-07-02 16:19:43 +0530

    Day-54 of learning

---
+++ video 54/script.js
@@ -0,0 +1,19 @@
+alert("good afternoon guys");
+
+console.log("hello");
+console.log("hello everyone");
+console.log("hello guys");
+console.log("hello friend");
+
+var a = prompt("enter your number");
+var isTrue = confirm("Do you want to close all tabs");
+
+if (isTrue) {
+  console.log("shut down the Computer");
+} else {
+  console.log("I am not sure");
+}
+console.log("your number is " + a);
+
+document.title = "hello mota bhai";
+document.body.style.backgroundColor = "red";
