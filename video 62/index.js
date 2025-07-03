7fe248391aa5db222126f0642013032b22fe5f40
Chahat verma <chahatverma360@gmail.com>
2025-07-03 18:11:43 +0530

    Day-62 of learning

---
+++ video 62/index.js
@@ -0,0 +1,19 @@
+/* create a business name generator by combining list of adjectives and shop name another word
+
+Adjectives :
+Crazy
+Amazing
+Fire
+
+Shop Name :
+Engine
+Foods
+Garments
+
+Another word :
+Bros
+Limited
+Hub
+
+Note : Don't use arrray
+*/
