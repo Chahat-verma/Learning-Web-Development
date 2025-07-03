e5671e5ada6f05b403792fd8500e6024eef25150
Chahat verma <chahatverma360@gmail.com>
2025-07-03 14:48:07 +0530

    Day-61 of learning(Solution of Exercise9)

--- video 61/index.js
+++ video 61/index.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+//Solution of exrcise 9
 console.log("Hello this is Solution of Exercise 9");
 
-let a = parseInt(prompt("Enter number 1"));
+let a = prompt("Enter number 1");
 let x = prompt("Enter operation");
-let b = parseInt(prompt("Enter number 2"));
-=======
-console.log("Hello this is Solution of Exercise 9")
-
-let a= parseInt(prompt("Enter number 1"));
-let b= parseInt(prompt("Enter number 2"));
-let x = prompt("Enter operation")[0];
->>>>>>> 891850a3ff9f84eadd954aff848691c15a9099b7
+let b = prompt("Enter number 2");
 let c = Math.random();
 // method 1
 // e = c * 10;
@@ -52,35 +45,17 @@ let c = Math.random();
 // }
 
 // method 2
-<<<<<<< HEAD
 let obj = {
-  "+": "-",
-  "*": "+",
-  "-": "/",
-  "/": "**",
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
 };
 if (c > 0.1) {
   //perform correct calculation
-  alert(`The result is ${eval(`${a} ${x} ${b}`)}`);
+alert(`The result is ${eval(`${a} ${x} ${b}`)}`);
 } else {
   //Perform wrong calculation
-  o = obj[o];
-  alert(`The result is ${eval(`${a} ${x} ${b}`)}`);
+    o = obj[o];
+    alert(`The result is ${eval(`${a} ${x} ${b}`)}`);
 }
-=======
-let obj ={
-    "+":"-",
-    "*":"+",
-    "-":"/",
-    "/":"**"
-}
-if(c>0.1){
-    //perform correct calculation
-    alert(`The result is ${eval(`${a} ${x} ${b}`)}`)
-}
-else{
-    //Perform wrong calculation
-    o = obj[o]
-    alert(`The result is ${eval(`${a} ${x} ${b}`)}`)
-}
->>>>>>> 891850a3ff9f84eadd954aff848691c15a9099b7
