3ca21d60315868ea5310a213d5bb619a353cfe29
Chahat verma <chahatverma360@gmail.com>
2025-07-03 14:30:39 +0530

    Day-61 of learning(Solution of exercise 9)

---
+++ video 61/index.js
@@ -0,0 +1,60 @@
+console.log("Hello this is Solution of Exercise 9");
+
+let a = parseInt(prompt("Enter number 1"));
+let x = prompt("Enter operation");
+let b = parseInt(prompt("Enter number 2"));
+let c = Math.random();
+// method 1
+// e = c * 10;
+// if (e%10){
+//     switch(x) {
+//         case '+':
+//             console.log(a-b);
+//             break;
+//         case '*':
+//             console.log(a+b);
+//             break;
+//         case '-':
+//             console.log(a/b);
+//             break;
+//         case '/':
+//             console.log(a**b);
+//             break;
+//         default:
+//             console.log("Enter a valid operation");
+//     }
+// }
+// else{
+//     switch(x) {
+//         case '+':
+//             console.log(a+b);
+//             break;
+//         case '*':
+//             console.log(a*b);
+//             break;
+//         case '-':
+//             console.log(a-b);
+//             break;
+//         case '/':
+//             console.log(a/b);
+//             break;
+//         default:
+//             console.log("Enter a valid operation");
+//     }
+// }
+
+// method 2
+let obj = {
+  "+": "-",
+  "*": "+",
+  "-": "/",
+  "/": "**",
+};
+if (c > 0.1) {
+  //perform correct calculation
+  alert(`The result is ${eval(`${a} ${x} ${b}`)}`);
+} else {
+  //Perform wrong calculation
+  o = obj[o];
+  alert(`The result is ${eval(`${a} ${x} ${b}`)}`);
+}
