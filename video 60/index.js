07873cfa2f47c7b6ddea7cb77388b67c5fed06e5
Chahat verma <chahatverma360@gmail.com>
2025-07-03 13:15:25 +0530

    Day-60 of learning

---
+++ video 60/index.js
@@ -0,0 +1,40 @@
+console.log("Hello this is tutuorial on strings in js")
+let n = "Chahat verma"
+console.log(n[0])
+console.log(n[1])
+console.log(n[2])
+console.log(n[3])
+console.log(n[4])
+console.log(n[5])
+console.log(n[6])
+console.log(n[7])
+console.log(n[8])
+console.log(n[9])
+console.log(n[10])
+console.log(n[11])
+console.log(n.length)
+
+let naam = "Chahat"
+let dost = "Rajat"
+console.log("My name is "+naam+" and my friend name is "+ dost )
+console.log(`My name is ${naam} and my friend name is ${dost}` )
+
+//escape sequence character
+// s ="aman"
+// s ="am'an"
+// s ="am"an" this show an error because it can't justify the ending of the string
+// s ="am\"an"//thie is an escape sequence character
+// s =`am"an`//thie is done by using template literals
+
+let r = "raju ram"
+console.log(r.toUpperCase())
+console.log(r.toLowerCase())
+console.log(r.length)
+console.log(r.slice(1,4))
+console.log(r.slice(1))
+
+console.log(r.replace("ju", 231 ))
+console.log(r.replace("ra", 231 ))// if occurance is more than one then it replace only the firsr one
+console.log(r.concat(n))
+console.log(r.concat("mohan", "jhandu"))
+//there are lots of methods of sr=tring to explore
