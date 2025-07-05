0e7f66c8fca31a0783b7e8fc9231575845b9cd81
Chahat verma <chahatverma360@gmail.com>
2025-07-05 17:28:16 +0530

    Day-67 of learning

---
+++ video 67/script.js
@@ -0,0 +1,16 @@
+console.log("Hello world")
+
+document.body.childNodes[0]
+document.body.childNodes[1]
+document.body.childNodes[1].childNodes
+document.body.childNodes[1].firstElementChild
+document.body.childNodes[1].firstElementChild.style.backgroundColor= "blue"
+document.body.childNodes[1].firstElementChild.parentElement
+document.body.firstElementChild.children[1]
+document.body.firstElementChild.children[2].nextElementSibling
+document.body.firstElementChild.children[2].previousElementSibling
+document.body.firstElementChild.children[2].previousSibling
+document.body.children
+document.body.children[1]
+document.body.children[1].rows
+document.body.children[1].columns
