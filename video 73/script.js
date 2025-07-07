d7de281e6b7dba4c79f2a082ece636c8a3e5571d
Chahat verma <chahatverma360@gmail.com>
2025-07-07 14:16:19 +0530

    Day-73 of learning

---
+++ video 73/script.js
@@ -0,0 +1,4 @@
+function createcard(title, cname, views, monthold, duration, thumbnail){
+
+}
+createcard("introduction to Backend", "CodeWithHarry", 1252200, 3, "32:44", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACwWOixJVrKLFindk92kYMgTcQbw")
