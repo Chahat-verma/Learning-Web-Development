904ddcdcf6c7269cd4bf7c85c2f5ca62daa91fb2
Chahat verma <chahatverma360@gmail.com>
2025-07-07 12:10:17 +0530

    Day-71 of learning

---
+++ video 71/script.js
@@ -0,0 +1,27 @@
+document.querySelector(".box")
+document.querySelector(".container").innerHTML
+document.querySelector(".box").innerHTML
+document.querySelector(".box").innerText
+document.querySelector(".container").innerText
+document.querySelector(".container").outer
+document.querySelector(".container").outerHTML
+document.querySelector(".container").tagName
+document.querySelector(".container").nodeName
+document.querySelector(".container").textContent
+document.querySelector(".container").hidden
+document.querySelector(".container").hidden = true
+document.querySelector(".box").innerHTML = "Ye box Khali h "
+document.querySelector(".box").hasAttribute("style")
+document.querySelector(".box").getAttribute("style")
+document.querySelector(".box").setAttribute("style")
+document.querySelector(".box").attributes
+document.querySelector(".box").removeAttribute("style")
+document.querySelector(".box").dataset
+document.querySelector(".box").remove()
+document.querySelector(".container").classList
+document.querySelector(".container").classname
+document.querySelector(".container").classList.add("gem")
+document.querySelector(".container").classList.remove("red")
+document.querySelector(".container").classList.toggle("red")
+
+// document.designMode = "on" to desgin any website on your Device
